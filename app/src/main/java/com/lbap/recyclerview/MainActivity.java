ba1b3065b32b2b98ed3b00aa4b53a0f3b19b9cd0
unknown <shantudashbd@yahoo.com>
2020-06-09 00:25:50 +0600

    recycler view added

--- app/src/main/java/com/lbap/recyclerview/MainActivity.java
+++ app/src/main/java/com/lbap/recyclerview/MainActivity.java
@@ -1,14 +1,34 @@
 package com.lbap.recyclerview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        initView();
+
+        setupRecyclerView();
+    }
+
+    private void initView() {
+        recyclerView = findViewById(R.id.recyclerView);
+    }
+
+    private void setupRecyclerView() {
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
     }
 }
